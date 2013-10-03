464c94402d8e3a649b58c8a0b600c0438232483b
inoue_yusuke <inoue.yusuke.928@gmail.com>
2013-10-03 20:52:45 +0900

    symlink.shを更新

--- symlink.sh
+++ symlink.sh
@@ -5,7 +5,7 @@ cd $(dirname $0)
 for dotfile in .?*
 do
   # exclude parent dir and .git
-  if [ $dotfile != ".." ] && [ $dotfile != ".git" ] && [ $dotfile != ".gitmodules"]
+  if [ $dotfile != ".." ] && [ $dotfile != ".git" ]
   then
     ln -Fis "$PWD/$dotfile" $HOME
   fi

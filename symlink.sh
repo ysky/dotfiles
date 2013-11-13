612d9957b3d022b0086d80bc72d8d62f5bd6dd51
inoue_yusuke <inoue.yusuke.928@gmail.com>
2013-11-13 11:58:58 +0900

    gitconfigを追加

--- symlink.sh
+++ symlink.sh
@@ -5,7 +5,7 @@ cd $(dirname $0)
 for dotfile in .?*
 do
   # exclude parent dir and .git
-  if [ $dotfile != ".." ] && [ $dotfile != ".git" ]
+  if [ $dotfile != ".." ] && [ $dotfile != ".git" ] && [ $dotfile != ".gitmodule" ]
   then
     ln -Fis "$PWD/$dotfile" $HOME
   fi

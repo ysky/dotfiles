ff06e69b47996fa08b2c0c564c20f890c2125061
inoue_yusuke <inoue.yusuke.928@gmail.com>
2015-02-12 10:37:40 +0900

    symlinkの作成スクリプトを修正

--- symlink.sh
+++ symlink.sh
@@ -2,10 +2,12 @@
 
 cd $(dirname $0)
 
+cp .gitconfig.tmpl .gitconfig
+
 for dotfile in .?*
 do
   # exclude parent dir and .git
-  if [ $dotfile != ".." ] && [ $dotfile != ".git" ] && [ $dotfile != ".gitmodule" ]
+  if [ $dotfile != ".." ] && [ $dotfile != ".git" ] && [ $dotfile != ".gitmodule" ] && [ $dotfile != ".gitconfig.tmpl" ]
   then
     ln -Fis "$PWD/$dotfile" $HOME
   fi

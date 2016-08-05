0906bc3b210f834321b6ec21992be26e292bad00
inoue_yusuke <inoue.yusuke.928@gmail.com>
2016-08-05 15:53:58 +0900

    control-B でブランチが表示インクリメンタルサーチできるように修正

---
+++ .zsh/peco-git-branches.zsh
@@ -0,0 +1,8 @@
+function peco-git-branches(){
+  local res=$(git branch -a | sed 's/^\*/ /'| awk '{ print $1 }' | peco)
+  BUFFER+=$res
+  zle clear-screen
+}
+
+zle -N peco-git-branches
+bindkey '^B' peco-git-branches
